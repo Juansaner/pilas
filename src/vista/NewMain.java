f8eb2a14efcaeaf25710c87ce0ccb38a805248af
mangel119 <mangel.sanabria119@gmail.com>
2021-03-03 17:29:09 -0500

    Tools

--- src/vista/NewMain.java
+++ src/vista/NewMain.java
@@ -5,7 +5,7 @@
  */
 package vista;
 
-import datos.Cliente;
+
 import pilas.Pila;
 
 /**
@@ -18,18 +18,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Pila<Cliente> pilaC=new Pila<>();
-        
-        pilaC.apilar(new Cliente("20145", "da@uao.edu.co", "David", "12000", "calle56"));
-        pilaC.apilar(new Cliente("2589", "ma@uao.edu.co", "Maria", "96325", "calle32"));
-        pilaC.apilar(new Cliente("3454", "rt@uao.edu.co", "Ramiro", "8547", "Avenida 6"));
-        
-        System.out.println("Datos en la pila original" + pilaC.toString());
-        
-        pilaC.desapilar();
-        
-        System.out.println("Datos en la pila original" + pilaC.toString());
+       
     }
     
 }
