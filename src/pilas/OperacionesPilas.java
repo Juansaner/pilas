73db198e778cb6d34ac517135c36654e5d6aa5b5
Juansaner <69471206+Juansaner@users.noreply.github.com>
2021-03-06 23:17:59 -0500

    OperacionesPilas
    Metodos concatenar - Invertir

--- src/pilas/OperacionesPilas.java
+++ src/pilas/OperacionesPilas.java
@@ -29,4 +29,28 @@ public class OperacionesPilas<T extends Base> {
         return duplicada;
     
     }
+    // invertir
+    public static <T extends Base> Pila<T> invertir(Pila<T> p) {
+        Pila<T> pilaInvertida = new Pila<>();
+        Pila<T> pilaAux = duplicarPila(p);
+        while (!pilaAux.estaVacia()) {
+            pilaInvertida.apilar(pilaAux.desapilar());
+        }
+        return pilaInvertida;
+    }
+    // concatenar
+    public static <T extends Base> Pila<T> concatenar(Pila<T> p1, Pila<T> p2) {
+        Pila<T> pilaAuxP = invertir(p1);
+        Pila<T> pilaAuxQ = invertir(p2);
+        Pila<T> pilaConcatenada = new Pila<>();
+        while (!pilaAuxP.estaVacia()) {
+            pilaConcatenada.apilar(pilaAuxP.desapilar());
+            
+        }
+        while (!pilaAuxQ.estaVacia()) {
+            pilaConcatenada.apilar(pilaAuxQ.desapilar());
+            
+        }
+        return pilaConcatenada;
+    }
 }
